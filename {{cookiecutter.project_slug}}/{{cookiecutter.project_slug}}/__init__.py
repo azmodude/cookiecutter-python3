d8e8d91aefc6ec97290b6eed3229fffd07aeac5d
Gordon Schulz <gordon.schulz@gmail.com>
2017-12-04 18:51:26 +0100

    Add pkg_resources version discovery

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for {{ cookiecutter.project_name }}."""
+
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except AttributeError:
+    __version__ = 'unknown'
 
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
